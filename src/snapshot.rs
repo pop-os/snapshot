3a1e23f9c4a5854b1e5831ceff76105d0522b1e2
Lucy <lucy@absolucy.moe>
2022-05-23 15:45:23 -0400

    ✨ Add `MountedBtrfs::list_snapshots`

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MPL-2.0
 use anyhow::{anyhow, Context, Result};
-use libbtrfsutil::{CreateSnapshotFlags, SubvolumeInfo, SubvolumeIterator, SubvolumeIteratorFlags};
+use libbtrfsutil::{CreateSnapshotFlags, SubvolumeIterator, SubvolumeIteratorFlags};
 use std::{
 	path::{Path, PathBuf},
-	time::SystemTime,
+	time::{Duration, SystemTime},
 };
 use sys_mount::{FilesystemType, Mount, UnmountDrop, UnmountFlags};
 use tempfile::TempDir;
+
+#[derive(Debug, Clone)]
+pub struct Snapshot {
+	capture_time: SystemTime,
+	path: PathBuf,
+	subvolumes: Vec<String>,
+}
 
 pub struct MountedBtrfs {
 	_mount: UnmountDrop<Mount>,
@@ -18,6 +25,60 @@ impl MountedBtrfs {
 		self.tempdir.path()
 	}
 
+	pub fn list_snapshots(&self) -> Result<Vec<Snapshot>> {
+		let mut snapshots = Vec::new();
+		let snapshot_dir = self.path().join("@snapshots").join("pop-snapshots");
+		if !snapshot_dir.exists() {
+			return Ok(Vec::new());
+		}
+		for entry in snapshot_dir
+			.read_dir()
+			.with_context(|| format!("failed to read directory {}", snapshot_dir.display()))?
+		{
+			let entry = entry
+				.with_context(|| format!("failed to read entry in {}", snapshot_dir.display()))?;
+			let path = entry.path();
+			if !path.exists() {
+				continue;
+			}
+			let capture_time = match path
+				.file_name()
+				.and_then(|file_name_os| file_name_os.to_str())
+				.and_then(|file_name| file_name.parse::<u64>().ok())
+				.map(|seconds| SystemTime::UNIX_EPOCH + Duration::from_secs(seconds))
+			{
+				Some(capture_time) => capture_time,
+				None => continue,
+			};
+			let mut subvolumes = Vec::new();
+			for entry in path
+				.read_dir()
+				.with_context(|| format!("failed to read directory {}", path.display()))?
+			{
+				let entry =
+					entry.with_context(|| format!("failed to read entry in {}", path.display()))?;
+				let path = entry.path();
+				if !path.exists() {
+					continue;
+				}
+				let name = match path
+					.file_name()
+					.and_then(|file_name_os| file_name_os.to_str())
+				{
+					Some(name) => name,
+					None => continue,
+				};
+				subvolumes.push(name.to_string());
+			}
+			snapshots.push(Snapshot {
+				capture_time,
+				path,
+				subvolumes,
+			});
+		}
+		Ok(snapshots)
+	}
+
 	pub fn make_snapshot(&self) -> Result<()> {
 		let subvolumes_to_snapshot =
 			get_non_home_subvolumes().context("failed to list subvolumes")?;
@@ -26,13 +87,20 @@ impl MountedBtrfs {
 			.duration_since(SystemTime::UNIX_EPOCH)
 			.context("failed to get current time")?
 			.as_secs();
-		let snapshots_dir = self.tempdir.path().join("@snapshots");
+		let snapshot_dir = self
+			.path()
+			.join("@snapshots")
+			.join("pop-snapshots")
+			.join(epoch.to_string());
 		for path in subvolumes_to_snapshot {
+			if !snapshot_dir.is_dir() {
+				std::fs::create_dir_all(&snapshot_dir).context("failed to create snapshot dir")?;
+			}
 			let snapshot_name = format!("pop-snapshot_{epoch}_{}", path.display());
 			println!("creating snapshot {snapshot_name}");
 			libbtrfsutil::create_snapshot(
 				self.path(),
-				&snapshots_dir.join(snapshot_name),
+				&snapshot_dir.join(snapshot_name),
 				CreateSnapshotFlags::READ_ONLY,
 				None,
 			)
