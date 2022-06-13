f41467a838914f4aa668e4e9a509d1dd594e2a3b
Lucy <lucy@absolucy.moe>
2022-06-13 13:33:48 -0400

    ✨ Create `@snapshots` subvolume if it does not exist.

--- service/src/snapshot/mount.rs
+++ service/src/snapshot/mount.rs
@@ -3,6 +3,7 @@
 use super::MountedBtrfs;
 use crate::util::find_root_device;
 use anyhow::{Context, Result};
+use libbtrfsutil::CreateSubvolumeFlags;
 use sys_mount::{FilesystemType, Mount, UnmountFlags};
 
 impl MountedBtrfs {
@@ -15,6 +16,7 @@ impl MountedBtrfs {
 			.context("failed to find root device")?;
 		debug!("Found root device path at {}", root_device_path.display());
 		let tempdir_path = tempdir.path().to_path_buf();
+		let snapshots_path = tempdir_path.join("@snapshots");
 
 		debug!(
 			"Mounting {}[subvol=/] at {}",
@@ -29,6 +31,18 @@ impl MountedBtrfs {
 		})
 		.await?
 		.context("failed to mount root subvolume")?;
+
+		if !snapshots_path.exists() {
+			let pop_snapshots_dir = snapshots_path.join("pop-snapshots");
+			tokio::task::spawn_blocking(move || {
+				libbtrfsutil::create_subvolume(snapshots_path, CreateSubvolumeFlags::empty(), None)
+			})
+			.await?
+			.context("failed to create snapshot subvolume")?;
+			tokio::fs::create_dir(&pop_snapshots_dir)
+				.await
+				.with_context(|| format!("failed to create {}", pop_snapshots_dir.display()))?;
+		}
 
 		Ok(MountedBtrfs {
 			_mount: mount,
